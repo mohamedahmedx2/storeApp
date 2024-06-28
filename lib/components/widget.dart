c95dcd12e684c0decd3bf9c6f02188a1f4b2b9da
mohamedahmedx2 <mohamedahmedt973@gmail.com>
2024-06-28 22:47:17 +0300

    firet commit

---
+++ lib/components/widget.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+import '../models/product_model.dart';
+import '../screens/update_product_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  CustomCard({
+    required this.product,
+    Key? key,
+  }) : super(key: key);
+
+  ProductModel product;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, UpdateProductPage.id ,arguments: product);
+      },
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                blurRadius: 50,
+                color: Colors.grey.withOpacity(.1),
+                spreadRadius: 20,
+                offset: Offset(10, 10),
+              ),
+            ]),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              elevation: 10,
+              child: Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.title.substring(0, 6),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 3,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          r'$' '${product.price.toString()}',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 32,
+            top: -60,
+            child: Image.network(
+              product.image,
+              height: 100,
+              width: 100,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+class CustomButon extends StatelessWidget {
+  CustomButon({this.onTap, required this.text});
+  VoidCallback? onTap;
+  String text;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        width: double.infinity,
+        height: 60,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField({this.hintText,this.inputType, this.onChanged, this.obscureText = false});
+  Function(String)? onChanged;
+  String? hintText;
+  TextInputType?  inputType;
+  bool? obscureText;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText!,
+      onChanged: onChanged,
+      keyboardType: inputType,
+      decoration: InputDecoration(
+        hintText: hintText,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+}
+
